4d2ef889a7443b4052a4ba3559216855b3fbbbf8
mindstorm8191 <mindstorm8191@yahoo.com>
2019-11-30 11:45:21 -0500

    First completed build
    I now have a working example to share. This took a whole week of trying various methods to achieve what I was after.

--- README.md
+++ README.md
@@ -1,68 +1,10 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## React-Three-Fiber Example
 
-## Available Scripts
+React-Three-Fiber is a library that allows you to create Three.js scenes using React commands.
+Check out the Git here: https://github.com/react-spring/react-three-fiber
 
-In the project directory, you can run:
+React-Three-Fiber has a bunch of demos to show you what it is capable of, and they're great.
+But they don't show you how to do basic things. This is what this example app is for.
 
-### `yarn start`
-
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
-
-### `yarn test`
-
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+This example shows you how to create a basic scene, and load models (from Blender) and
+textures. We are even able to rotate our model.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+    React-Three-Fiber example by Danny Bartsch
+    In this example, we will set up an environment, with a ship model loaded. We will then apply
+    a texture to it, and provide a starry background.
+*/
+
+import React, { Suspense, useRef } from "react";
+/* Note: The currently active version of React does NOT work with React-Three-Fiber. You must
+    specify a different version through package.json. This is currently using version
+    ^0.0.0-experimental-5faf377df */
+
+import * as THREE from "three";
+import { GLTFLoader } from "./GLTFLoader.js";
+import { Canvas, useFrame, useLoader } from "react-three-fiber";
+
+import shipTexture from "./steelwall2.jpg";
+import background from "./background2.png";
+import shipmodel from "./ship3.glb";
 
 function App() {
+  console.log("Lets get moving...");
+
+  /* Our app starts with a Three Canvas component. Note that Three primitives after that point are
+        not PascalCased, but Canvas is capitalized. Within the Canvas declaration, we define our camera,
+        and other properties.*/
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "95vh" }}>
+      <Canvas camera={{ position: [0, 0, 15] }} shadowMap="true" style={{ backgroundColor: "black" }}>
+        {/*Set up a light source. SpotLight shines in all directions from a single source */}
+        <spotLight intensity={0.6} position={[30, 30, 50]} angle={0.2} penumbra={1} castShadow />
+
+        {/* To render our ship, we need to provide a Suspense component first. From my
+            understanding, generating the model isn't done on right away; it is 
+            loaded asynchronously. Therefore, something must be provided until the
+            model loading is complete. Null is suitable here, or another type of object
+            can be used, too */}
+        <Suspense fallback={null}>
+          <MyShip />
+        </Suspense>
+
+        {/* Now show a starry background. Yes, we can pass props to our Three components,
+                just like normal React! */}
+        <StarryBackground texture={background} />
+      </Canvas>
     </div>
+  );
+}
+
+function StarryBackground(props) {
+  // Start by setting up our texture.
+  const texmgr = new THREE.TextureLoader();
+  const texture = texmgr.load(props.texture);
+
+  /* Since our sphere is so large, and the texture is not very detailed, we need
+        to set up texture wrapping. You will probably notice the image looking mirrored 
+        in some locations. The texture will also looked stretched at the corners; this is
+        a product of using the sphere.*/
+  texture.wrapS = texture.wrapT = THREE.MirroredRepeatWrapping;
+  texture.repeat.set(5, 5);
+
+  return (
+    <mesh>
+      <sphereBufferGeometry attach="geometry" args={[100, 64, 64]} />
+      {/* Since our camera will sit INSIDE the sphere, the sphere cannot be seen with
+                it, normally. We can change the side parameter to flip our mesh inside-out */}
+      <meshBasicMaterial attach="material" map={texture} side={THREE.BackSide} />
+    </mesh>
+  );
+}
+
+function MyShip() {
+  /* Start by calling useLoader. This will return an object, containing lots of data. GLTF is
+        designed to provide a whole scene, so selecting your object within that scene may take
+        some trial & error. For my ship, it was the 4th object.*/
+  const gltf = useLoader(GLTFLoader, shipmodel);
+
+  /* Next, we want to apply a texture to our ship. I was unable to load it directly through the
+        GLTF model, but UV positioning is still preserved, so we can apply it manually.
+        Textures are loaded through URL paths. Within a node.js environment, it is easier to import 
+        such files instead of fetching them.*/
+  const texmgr = new THREE.TextureLoader();
+  const texture = texmgr.load(shipTexture);
+
+  /* We want our object to rotate. In order to update our object after generating it, we need to
+        create a reference to it. This reference can be attached to any component of our structure.
+        We can then pass our function to useFrame.*/
+  const Reference = useRef();
+  useFrame(() => {
+    Reference.current.rotation.x += 0.005;
+    Reference.current.rotation.y += 0.005;
+  });
+
+  return (
+    <mesh ref={Reference}>
+      <bufferGeometry attach="geometry" {...gltf.__$[3].geometry} />
+      {/* If seeing your model is an issue because of lighting, use meshBasicMaterial here
+                instead. That material type ignores lighting all-together */}
+      <meshStandardMaterial attach="material" map={texture} />
+    </mesh>
   );
 }
 
